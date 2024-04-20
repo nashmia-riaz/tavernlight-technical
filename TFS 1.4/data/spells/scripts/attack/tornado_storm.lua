6de5d6f576f48770a9faa7994f53f4a78dd11517
Nashmia Riaz <nashmia-riaz@hotmail.com>
2024-04-20 19:16:41 +0500

    Added answers for Q5 - 'cast tornado storm' to use

---
+++ TFS 1.4/data/spells/scripts/attack/tornado_storm.lua
@@ -0,0 +1,46 @@
+-- approach is to split the spell into multiple smaller spells
+
+local combat1 = Combat()
+combat1:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat1:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat1:setArea(createCombatArea(AREA_TORNADO_1))
+
+local combat2 = Combat()
+combat2:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat2:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat2:setArea(createCombatArea(AREA_TORNADO_2))
+
+local combat3 = Combat()
+combat3:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat3:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat3:setArea(createCombatArea(AREA_TORNADO_3))
+
+local combat4 = Combat()
+combat4:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat4:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat4:setArea(createCombatArea(AREA_TORNADO_4))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+function onCastSpell(creature, variant)
+	createCombats(creature.uid, variant)
+	addEvent(createCombats, 1000, creature.uid, variant)
+	addEvent(createCombats, 2000, creature.uid, variant)
+	addEvent(createCombats, 3000, creature.uid, variant)
+	return true
+end
+
+function createCombats(cid, variant)
+	combat1:execute(cid, variant)
+	addEvent(executeCombat, 250, combat2, cid, variant)
+	addEvent(executeCombat, 500, combat3, cid, variant)
+	addEvent(executeCombat, 750, combat4, cid, variant)
+end
+
+function executeCombat(combat, cid, variant)
+	combat:execute(cid, variant)
+end
